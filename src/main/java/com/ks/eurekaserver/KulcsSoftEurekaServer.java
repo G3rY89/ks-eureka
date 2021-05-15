51a6ae1155442c56747a15971372b66cb1a80b6a
Qdog89 <gery89@gmail.com>
2021-05-15 20:09:08 +0200

    necessary changes to update spring

--- src/main/java/com/ks/eurekaserver/KulcsSoftEurekaServer.java
+++ src/main/java/com/ks/eurekaserver/KulcsSoftEurekaServer.java
@@ -2,6 +2,7 @@ package com.ks.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
